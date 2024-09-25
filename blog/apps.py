87c177b1087b6b3fe4e78076c93d5e82cfd3d6a3
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-09-25 09:01:56 +0300

    docstring added for apps.py and forms.py

--- blog/apps.py
+++ blog/apps.py
@@ -1,6 +1,19 @@
+"""
+The configuration for the blog application.
+
+This module contains the configuration for the blog application. The
+configuration is used by Django to load the application.
+
+"""
 from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    """
+    The configuration for the blog application.
+
+    This class is used to configure the application. It is used by Django
+    to load the application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,29 @@
+"""
+Forms for the blog application.
+
+This module contains the forms for the blog application. The forms are
+used to validate the user input.
+
+The forms are:
+
+    - EmailPostForm: a form to send a post by email to a friend
+    - CommentForm: a form to validate a comment model
+
+"""
 from django import forms
 from .models import Comment
 
 
 class EmailPostForm(forms.Form):
+    """
+    Form to send a post by email to a friend.
+
+    Fields:
+        - name: name of the person sending the email
+        - email: email address of the person sending the email
+        - to: email address of the person to send the email to
+        - comments: additional comments to add to the email
+    """
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
@@ -13,6 +34,21 @@ class EmailPostForm(forms.Form):
 
 
 class CommentForm(forms.ModelForm):
+    """
+    Form to validate a comment model.
+
+    Fields:
+        - name: name of the person posting the comment
+        - email: email address of the person posting the comment
+        - body: the text of the comment
+    """
     class Meta:
+        """
+        Meta class for CommentForm.
+
+        Attributes:
+            - model: the model the form is for
+            - fields: the fields to include in the form
+        """
         model = Comment
         fields = ('name', 'email', 'body')
