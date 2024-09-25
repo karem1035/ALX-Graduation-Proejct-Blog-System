348d9ddd5bdf24421dd07ca7c16bee3d50baa8f2
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-09-25 06:49:53 +0300

    added commenting section we can add comment now using each post added form to each post

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,10 @@
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 
 
 def post_detail(request, year, month, day, post):
@@ -16,9 +17,19 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+
     return render(
         request, 'blog/post/detail.html',
-        {'post': post}
+        {
+            'post': post,
+            'comments': comments,
+            'form': form
+        }
     )
 
 
@@ -76,5 +87,33 @@ def post_share(request, post_id):
             'post': post,
             'form': form,
             'sent': sent
+        }
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
         }
     )
