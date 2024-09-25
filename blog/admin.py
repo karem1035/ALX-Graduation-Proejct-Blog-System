fc5d6b4f7398f6f6583312f8129b2d05bd11047c
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-09-25 08:58:05 +0300

    added docstring to the admin.py classes

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,20 @@
+"""
+Admin classes for the blog application.
+
+This module contains the admin classes for the models in the blog
+application. The admin classes are used to customize the admin interface
+for the models.
+
+"""
 from django.contrib import admin
 from .models import Comment, Post
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    """
+    Admin class for Post model.
+    """
     list_display = ['title', 'slug', 'author', 'publish', 'status']
     list_filter = ['status', 'created', 'publish', 'author']
     search_fields = ['title', 'body']
@@ -16,6 +27,9 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    """
+    Admin class for Comment model.
+    """
     list_display = ['name', 'email', 'post', 'created', 'active']
     list_filter = ['active', 'created', 'updated']
     search_fields = ['name', 'email', 'body']
